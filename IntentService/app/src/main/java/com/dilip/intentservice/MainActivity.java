8f63c74333557364481d04b04c44253e9f2eecf4
Dilip Patel <pateldilip2902@gmail.com>
2024-07-12 15:41:33 +0530

    IntentService

---
+++ IntentService/app/src/main/java/com/dilip/intentservice/MainActivity.java
@@ -0,0 +1,108 @@
+package com.dilip.intentservice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.dilip.intentservice.services.MyDownloadService;
+import com.dilip.intentservice.services.MyIntentService;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyTag";
+    public static final String MESSAGE_KEY = "message_key";
+    private ScrollView mScroll;
+    private TextView mLog;
+    private ProgressBar mProgressBar;
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String songName = intent.getStringExtra(MESSAGE_KEY);
+            log(songName + " Downloaded...");
+
+            Log.d(TAG, "onReceive: Thread Name: " + Thread.currentThread().getName());
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        LocalBroadcastManager.getInstance(getApplicationContext())
+                .registerReceiver(mReceiver, new IntentFilter(DownloadHandler.SERVICE_MESSAGE));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        LocalBroadcastManager.getInstance(getApplicationContext())
+                .unregisterReceiver(mReceiver);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+    }
+
+    public void runCode(View v) {
+        log("Running code");
+        displayProgressBar(true);
+
+        //send intent to download service
+
+        for (String song : Playlist.songs) {
+            Intent intent = new Intent(MainActivity.this, MyIntentService.class);
+            intent.putExtra(MESSAGE_KEY, song);
+
+            startService(intent);
+        }
+
+    }
+
+    private void initViews() {
+        mScroll = findViewById(R.id.scrollLog);
+        mLog = findViewById(R.id.tvLog);
+        mProgressBar = findViewById(R.id.progress_bar);
+    }
+
+    public void clearOutput(View v) {
+        Intent intent = new Intent(MainActivity.this, MyDownloadService.class);
+        stopService(intent);
+
+        mLog.setText("");
+        scrollTextToEnd();
+    }
+
+    public void log(String message) {
+        Log.i(TAG, message);
+        mLog.append(message + "\n");
+        scrollTextToEnd();
+    }
+
+    private void scrollTextToEnd() {
+        mScroll.post(() -> mScroll.fullScroll(ScrollView.FOCUS_DOWN));
+    }
+
+    public void displayProgressBar(boolean display) {
+        if (display) {
+            mProgressBar.setVisibility(View.VISIBLE);
+        } else {
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+}
---
+++ IntentService/app/src/main/java/com/dilip/intentservice/Playlist.java
@@ -0,0 +1,5 @@
+package com.dilip.intentservice;
+
+public class Playlist {
+    public static String[] songs = {"Lehra Do", "Kaise Hua", "Daylight"};
+}
